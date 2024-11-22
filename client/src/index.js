02d7c30b885a71c6f1dc4b5165b3c7d288183143
pratik <routpratik09@gmail.com>
2024-11-22 06:06:11 +0530

    final push

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,54 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { createClient } from '@supabase/supabase-js';
+// import { SessionContextProvider } from '@supabase/auth-helpers-react'
+
+
+// const supabase = createClient(
+//   "https://pyucuqxbhewkkrmjbbqb.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5dWN1cXhiaGV3a2tybWpiYnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyMDgzMTAsImV4cCI6MjA0Nzc4NDMxMH0.yBgwlHoYNqW-UcN-EBDu7DxXlUx7-FlBfya6DuXAHEo"
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <SessionContextProvider supabaseClient={supabase}>
+//     <App />
+//     </SessionContextProvider>
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createClient } from '@supabase/supabase-js';
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 
 const supabase = createClient(
-  "https://pyucuqxbhewkkrmjbbqb.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5dWN1cXhiaGV3a2tybWpiYnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyMDgzMTAsImV4cCI6MjA0Nzc4NDMxMH0.yBgwlHoYNqW-UcN-EBDu7DxXlUx7-FlBfya6DuXAHEo"
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SessionContextProvider supabaseClient={supabase}>
-    <App />
+      <App />
     </SessionContextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
